6109d45213a49ca33c2bb3706f9f01bbee3b85f0
MatteoCantafio25 <mattyteo004@gmail.com>
2024-05-15 17:31:28 +0200

    Fixed FizzBuzz exercise

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -4,18 +4,13 @@ public class FizzBuzz {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0) {
-                System.out.println("Fizz");
-            }
-            if (i % 5 == 0) {
-                System.out.println("Buzz");
-            }
-            if ((i % 3 == 0) && (i % 5 == 0)) {
+            if (i % 15 == 0) {
                 System.out.println("FizzBuzz");
-            }
-            if (!(i % 3 == 0) && !(i % 5 == 0)) {
-                System.out.println(i);
-            }
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else System.out.println(i);
         }
     }
 }
