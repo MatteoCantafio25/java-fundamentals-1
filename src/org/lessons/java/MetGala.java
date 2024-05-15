31ac8d10cd91ec135ca6c8f9474a97f1b987678f
MatteoCantafio25 <mattyteo004@gmail.com>
2024-05-15 16:05:24 +0200

    MetGala exercise completed

---
+++ src/org/lessons/java/MetGala.java
@@ -0,0 +1,31 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class MetGala {
+    public static void main(String[] args) {
+        String userName;
+
+        String[] authorizedGuests = {"Jennifer Lopez", "Zendaya", "Chris Hemsworth", "Bad Bunny", "Cara Delevingne", "Kendall Jenner", "Uma Thurman", "Damiano David", "Luca Guadagnino"};
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Give me your name please");
+        userName = in.nextLine();
+
+        boolean isFound = false;
+
+        for (int i = 0; i < authorizedGuests.length; i++) {
+            if (userName.equals(authorizedGuests[i])) {
+                isFound = true;
+                break;
+            }
+        }
+
+        if (isFound) {
+            System.out.println("I found you on the list, you're welcome to access to the event.");
+        } else {
+            System.out.println("You are not on the list you can step away from the queue.");
+        }
+    }
+}
